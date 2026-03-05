6ef79a671d12f86045ad33a2c8d3e2ae406c8f2e
swaliha shaikh <salushaikh1410@gmail.com>
2026-03-06 00:05:11 +0530

    Add files via upload

---
+++ BASIC_CALC/script.js
@@ -0,0 +1,26 @@
+let display=document.getElementById("display");
+let buttons=document.querySelectorAll("button");
+
+function append(value)
+{
+    display.value+=value;
+}
+function calculate()
+{
+    try{
+        display.value=eval(display.value);
+    }
+    catch
+    {
+        display.value="Error!!"
+    }
+}
+function cleardisplay()
+{
+    display.value="";
+}
+function deletelast()
+{
+    display.value=display.value.slice(0,-1)
+
+}
